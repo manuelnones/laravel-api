b88455cbf48e791e6c77ed025c22490880d120d3
manuelnones <manuelnones99@yahoo.com>
2023-05-29 14:05:17 +0200

    add type + technologies to funcion index in PostController

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
     public function index() {
-        $posts = Post::all();
+        $posts = Post::with('type', 'technologies')->get();
 
         return response()->json([
             'results' => $posts,
